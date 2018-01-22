17571bb27b18172d913b96fa963e46ab108babd0
Elfansoer <elfansoer@gmail.com>
2018-01-22 15:14:03 +0700

    Create antimage_spell_shield.lua

---
+++ scripts/vscripts/lua_abilities/antimage_spell_shield/antimage_spell_shield.lua
@@ -0,0 +1,10 @@
+antimage_spell_shield = class({})
+LinkLuaModifier( "modifier_antimage_spell_shield", "lua_abilities/antimage_spell_shield/modifier_antimage_spell_shield", LUA_MODIFIER_MOTION_NONE )
+
+--------------------------------------------------------------------------------
+
+function antimage_spell_shield:GetIntrinsicModifierName()
+	return "modifier_antimage_spell_shield"
+end
+
+--------------------------------------------------------------------------------
