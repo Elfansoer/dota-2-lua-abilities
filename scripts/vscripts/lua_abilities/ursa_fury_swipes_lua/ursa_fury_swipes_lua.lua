59ed3a1f73fdf74bc44338c6877d00c86dd24cff
Elfansoer <elfansoer@gmail.com>
2018-01-23 14:22:44 +0700

    Create ursa_fury_swipes_lua.lua

---
+++ scripts/vscripts/lua_abilities/ursa_fury_swipes_lua/ursa_fury_swipes_lua.lua
@@ -0,0 +1,8 @@
+ursa_fury_swipes_lua = class({})
+LinkLuaModifier( "modifier_ursa_fury_swipes_lua", "lua_abilities/ursa_fury_swipes_lua/modifier_ursa_fury_swipes_lua", LUA_MODIFIER_MOTION_NONE )
+
+--------------------------------------------------------------------------------
+-- Passive Modifier
+function ursa_fury_swipes_lua:GetIntrinsicModifierName()
+	return "modifier_ursa_fury_swipes_lua"
+end
