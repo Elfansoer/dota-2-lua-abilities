0dd3f2a298c0babe5e6fac805ebf47a2b9c53ca3
Elfansoer <elfansoer@gmail.com>
2018-01-25 17:44:10 +0700

    Update test.lua

--- scripts/vscripts/lua_abilities/_test/test.lua
+++ scripts/vscripts/lua_abilities/_test/test.lua
@@ -10,6 +10,8 @@ end
 --[[
 Invoking results:
 [none]
+	MODIFIER_PROPERTY_MAGICAL_CONSTANT_BLOCK
+	MODIFIER_PROPERTY_INCOMING_PHYSICAL_DAMAGE_CONSTANT
 	MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE_PROC
 	MODIFIER_PROPERTY_PREATTACK_TARGET_CRITICALSTRIKE
 
@@ -74,10 +76,14 @@ Invoking results:
 	MODIFIER_PROPERTY_PHYSICAL_CONSTANT_BLOCK
 	MODIFIER_PROPERTY_INCOMING_SPELL_DAMAGE_CONSTANT
 	MODIFIER_PROPERTY_INCOMING_PHYSICAL_DAMAGE_PERCENTAGE
+	MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PHYSICAL
+	MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_MAGICAL
+	MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PURE
 	MODIFIER_PROPERTY_INCOMING_DAMAGE_PERCENTAGE
 	MODIFIER_PROPERTY_AVOID_DAMAGE
 	MODIFIER_PROPERTY_TOTAL_CONSTANT_BLOCK
 	MODIFIER_PROPERTY_PHYSICAL_CONSTANT_BLOCK
+	MODIFIER_PROPERTY_MIN_HEALTH
 
 [two-script attack]
 	[animation started] ---------------------------------------------------------------------
@@ -101,114 +107,130 @@ Invoking results:
 
 		MODIFIER_PROPERTY_PHYSICAL_CONSTANT_BLOCK
 		MODIFIER_PROPERTY_INCOMING_SPELL_DAMAGE_CONSTANT
+		MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS
 		MODIFIER_PROPERTY_INCOMING_PHYSICAL_DAMAGE_PERCENTAGE
+		MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PHYSICAL
+		MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_MAGICAL
+		MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PURE
+		MODIFIER_PROPERTY_MAGICAL_RESISTANCE_BONUS
 		MODIFIER_PROPERTY_INCOMING_DAMAGE_PERCENTAGE
 	MODIFIER_PROPERTY_TOTALDAMAGEOUTGOING_PERCENTAGE
 		MODIFIER_PROPERTY_AVOID_DAMAGE
 		MODIFIER_PROPERTY_TOTAL_CONSTANT_BLOCK
 	MODIFIER_EVENT_ON_ATTACKED
-		MODIFIER_PROPERTY_PHYSICAL_CONSTANT_BLOCK
+		MODIFIER_PROPERTY_MIN_HEALTH
 	MODIFIER_EVENT_ON_TAKEDAMAGE
 
+		?? (before min-health) MODIFIER_PROPERTY_PHYSICAL_CONSTANT_BLOCK
 
-[ability damage]
+[dealing ability]
 	MODIFIER_PROPERTY_TOTALDAMAGEOUTGOING_PERCENTAGE
 	MODIFIER_EVENT_ON_TAKEDAMAGE
 
-[attack with: 100 base, 100 bonus-damage-physical]
-[VScript] MODIFIER_PROPERTY_PROCATTACK_BONUS_DAMAGE_PHYSICAL
-[VScript] MODIFIER_EVENT_ON_ATTACK_LANDED
-[VScript] attacker	table: 0x00177690
-[VScript] unit	nil
-[VScript] target	table: 0x001bd008
-[VScript] damage	100
-[VScript] original_damage	100
-[VScript] damage_type	1
-[VScript] MODIFIER_EVENT_ON_ATTACKED
-[VScript] attacker	table: 0x00177690
-[VScript] unit	nil
-[VScript] target	table: 0x001bd008
-[VScript] damage	200
-[VScript] original_damage	200
-[VScript] damage_type	1
-[VScript] MODIFIER_EVENT_ON_TAKEDAMAGE
-[VScript] attacker	table: 0x00177690
-[VScript] unit	table: 0x001bd008
-[VScript] target	nil
-[VScript] damage	200
-[VScript] original_damage	200
-[VScript] damage_type	1
+[struck ability]
+	MODIFIER_PROPERTY_PHYSICAL_CONSTANT_BLOCK
+	MODIFIER_PROPERTY_INCOMING_SPELL_DAMAGE_CONSTANT
+	MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PHYSICAL
+	MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_MAGICAL
+	MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PURE
+	MODIFIER_PROPERTY_INCOMING_DAMAGE_PERCENTAGE
+	MODIFIER_PROPERTY_AVOID_DAMAGE
+	MODIFIER_PROPERTY_TOTAL_CONSTANT_BLOCK
+	MODIFIER_PROPERTY_MIN_HEALTH
 
-[attack with: 100 base, 200%-crit, 100 bonus-damage-physical]
-[VScript] MODIFIER_PROPERTY_PREATTACK_CRITICALSTRIKE
-[VScript] MODIFIER_PROPERTY_PROCATTACK_BONUS_DAMAGE_PHYSICAL
-[VScript] MODIFIER_EVENT_ON_ATTACK_LANDED
-[VScript] attacker	table: 0x00177690
-[VScript] unit	nil
-[VScript] target	table: 0x00163d58
-[VScript] damage	300
-[VScript] original_damage	300
-[VScript] damage_type	1
-[VScript] MODIFIER_EVENT_ON_ATTACKED
-[VScript] attacker	table: 0x00177690
-[VScript] unit	nil
-[VScript] target	table: 0x00163d58
-[VScript] damage	400
-[VScript] original_damage	400
-[VScript] damage_type	1
-[VScript] MODIFIER_EVENT_ON_TAKEDAMAGE
-[VScript] attacker	table: 0x00177690
-[VScript] unit	table: 0x00163d58
-[VScript] target	nil
-[VScript] damage	400
-[VScript] original_damage	400
-[VScript] damage_type	1
+[two-script ability]
+		MODIFIER_PROPERTY_PHYSICAL_CONSTANT_BLOCK
+		MODIFIER_PROPERTY_INCOMING_SPELL_DAMAGE_CONSTANT
+		MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PHYSICAL
+		MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_MAGICAL
+		MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PURE
+		MODIFIER_PROPERTY_INCOMING_DAMAGE_PERCENTAGE
+	MODIFIER_PROPERTY_TOTALDAMAGEOUTGOING_PERCENTAGE
+		MODIFIER_PROPERTY_AVOID_DAMAGE
+		MODIFIER_PROPERTY_TOTAL_CONSTANT_BLOCK
+		MODIFIER_PROPERTY_MIN_HEALTH
+	MODIFIER_EVENT_ON_TAKEDAMAGE
 
-[attack with: 100 base, 100 bonus-damage-magical]
-[VScript] MODIFIER_PROPERTY_PROCATTACK_BONUS_DAMAGE_MAGICAL
-[VScript] MODIFIER_EVENT_ON_ATTACK_LANDED
-[VScript] attacker	table: 0x00177690
-[VScript] unit	nil
-[VScript] target	table: 0x001a0568
-[VScript] damage	100
-[VScript] original_damage	100
-[VScript] damage_type	1
-[VScript] MODIFIER_EVENT_ON_ATTACKED
-[VScript] attacker	table: 0x00177690
-[VScript] unit	nil
-[VScript] target	table: 0x001a0568
-[VScript] damage	163.75
-[VScript] original_damage	200
-[VScript] damage_type	1
-[VScript] MODIFIER_EVENT_ON_TAKEDAMAGE
-[VScript] attacker	table: 0x00177690
-[VScript] unit	table: 0x001a0568
-[VScript] target	nil
-[VScript] damage	163.75
-[VScript] original_damage	200
-[VScript] damage_type	1
+[Damaging calculations]
+B00: Base damage
+BBD: Base bonus damage
+TBD: Total base damage
+BML: Base multiplier
+FBD: Flat bonus damage
+TFB: Total flat bonus damage
+TML: Total multiplier
+DPC: Total Damage pre-crit
+CRM: Critical multiplier
+BPC: Bonus damage post-crit
+FDD: Final damage delivered
+BDP: Bonus Damage Physical
+BDM: Bonus Damage Magical
+BDR: Bonus Damage Pure
 
-[attack with: 100 base damage, 100% total-outgoing]
-[VScript] MODIFIER_EVENT_ON_ATTACK_LANDED
-[VScript] attacker	table: 0x00177690
-[VScript] unit	nil
-[VScript] target	table: 0x0019ec58
-[VScript] damage	100
-[VScript] original_damage	100
-[VScript] damage_type	1
-[VScript] MODIFIER_PROPERTY_TOTALDAMAGEOUTGOING_PERCENTAGE
-[VScript] MODIFIER_EVENT_ON_ATTACKED
-[VScript] attacker	table: 0x00177690
-[VScript] unit	nil
-[VScript] target	table: 0x0019ec58
-[VScript] damage	200
-[VScript] original_damage	100
-[VScript] damage_type	1
-[VScript] MODIFIER_EVENT_ON_TAKEDAMAGE
-[VScript] attacker	table: 0x00177690
-[VScript] unit	table: 0x0019ec58
-[VScript] target	nil
-[VScript] damage	200
-[VScript] original_damage	100
-[VScript] damage_type	1
+base damage		bonus damage
+-- base hero damage
+B00
+
+-- base bonus damage
++BBD
+B00+BBD=TBD
+
+-- base multiplier
++0			+(TBD)*BML
+TBD			TBD*BML
+
+-- flat bonus damage
++0			+FBD
+TBD			TBD*BML + FBD = TFB
+
+-- total multiplier
++0			+(TBD + TFB)*TML
+TBD			TFB + (TBD+TFB)*TML
+
+-- damage pre-crit
+DPC
+
+-- critical multiplier
++DPC*(1-CRM)
+DPC*CRM
+
+-- bonus damage post-crit
++BPC
+DPC*CRM + BPC
+
+-- Final attack damage before delivered
+FDD
+
+-- Add bonus damage: physical, magical, pure
++BDP		+BDM		+BDR
+FDD+BDP		BDM			BDR
+ADP			ADM			ADP
+
+-- constant physical block
+-CPB		0			0
+
+-- armor reduction
+-ARP		0			0
+
+-- physical bonus percentage
++PBP 		0			0
+
+-- spell damage constant
+0			+SDC 		0
+
+-- magic resistance
+0			-MRP 		0
+
+-- total damage before special manipulation
+TAD
+
+-- incoming damage percentage
+TAD*IDP
+
+-- outgoing damage percentage
+TAD*IDP*ODP
+
+-- final constant block
+TAD*IDP*ODP - FCB
+FINAL
 ]]
