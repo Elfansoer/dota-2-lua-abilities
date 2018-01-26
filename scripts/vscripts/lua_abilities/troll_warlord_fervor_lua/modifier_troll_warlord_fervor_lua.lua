c84e35cf57be3beafdf5aa30c9548ce8f41cc8ff
Elfansoer <elfansoer@gmail.com>
2018-01-26 11:30:36 +0700

    Update modifier_troll_warlord_fervor_lua.lua

--- scripts/vscripts/lua_abilities/troll_warlord_fervor_lua/modifier_troll_warlord_fervor_lua.lua
+++ scripts/vscripts/lua_abilities/troll_warlord_fervor_lua/modifier_troll_warlord_fervor_lua.lua
@@ -1,35 +1,55 @@
 modifier_troll_warlord_fervor_lua = class({})
 
-function modifier_troll_warlord_fervor_lua:OnCreated( kv )
-	if IsServer() then
-		self:SetStackCount(1)
-		self.stack_multiplier = self:GetAbility():GetSpecialValueFor("attack_speed")
-		self.max_stacks = self:GetAbility():GetSpecialValueFor("max_stacks")
-		self.currentTarget = {}
-	end
+--------------------------------------------------------------------------------
+-- Initializations
+
+function modifier_troll_warlord_fervor_lua:IsHidden( kv )
+	return false
 end
 
-function modifier_troll_warlord_fervor_lua:OnRefresh( kv )
-	if IsServer() then
-		self.stack_multiplier = self:GetAbility():GetSpecialValueFor("attack_speed")
-		self.max_stacks = self:GetAbility():GetSpecialValueFor("max_stacks")
-	end
+function modifier_troll_warlord_fervor_lua:IsDebuff( kv )
+	return false
+end
+
+function modifier_troll_warlord_fervor_lua:IsPurgable( kv )
+	return false
+end
+
+function modifier_troll_warlord_fervor_lua:RemoveOnDeath( kv )
+	return false
 end
 
 --------------------------------------------------------------------------------
+-- Life cycle
+
+function modifier_troll_warlord_fervor_lua:OnCreated( kv )
+	self:SetStackCount(1)
+	self.stack_multiplier = self:GetAbility():GetSpecialValueFor("attack_speed")
+	self.max_stacks = self:GetAbility():GetSpecialValueFor("max_stacks")
+	self.currentTarget = {}
+end
+
+function modifier_troll_warlord_fervor_lua:OnRefresh( kv )
+	self.stack_multiplier = self:GetAbility():GetSpecialValueFor("attack_speed")
+	self.max_stacks = self:GetAbility():GetSpecialValueFor("max_stacks")
+end
+
+--------------------------------------------------------------------------------
+-- Declare functions
 
 function modifier_troll_warlord_fervor_lua:DeclareFunctions()
 	local funcs = {
 		MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT,
-		MODIFIER_EVENT_ON_ATTACK_START
+		MODIFIER_EVENT_ON_ATTACK
 	}
 
 	return funcs
 end
 
 --------------------------------------------------------------------------------
+-- Declared functions
 
-function modifier_troll_warlord_fervor_lua:OnAttackStart( params )
+function modifier_troll_warlord_fervor_lua:OnAttack( params )
 	if IsServer() then
 		-- filter
 		pass = false
@@ -50,15 +70,22 @@ function modifier_troll_warlord_fervor_lua:OnAttackStart( params )
 end
 
 function modifier_troll_warlord_fervor_lua:GetModifierAttackSpeedBonus_Constant( params )
-	if IsServer() then
-		return self:GetStackCount() * self.stack_multiplier
+	local passive = 1
+	if self:GetParent():PassivesDisabled() then
+		passive = 0
 	end
+	return self:GetStackCount() * self.stack_multiplier * passive
 end
+
+--------------------------------------------------------------------------------
+-- Helper functions
 
 function modifier_troll_warlord_fervor_lua:AddStack()
 	-- check if it is not maximum
-	if self:GetStackCount < self.max_stacks then
-		self:IncrementStackCount()
+	if not self:GetParent():PassivesDisabled() then
+		if self:GetStackCount < self.max_stacks then
+			self:IncrementStackCount()
+		end
 	end
 end
 
