db65414580d0d12cdc0d2172352d81e051c430cc
Elfansoer <elfansoer@gmail.com>
2018-01-26 11:00:02 +0700

    Update and rename scripts/vscripts/lua_abilities/troll_warlord_fervor/troll_warlord_fervor.lua to scripts/vscripts/lua_abilities/troll_warlord_fervor_lua/troll_warlord_fervor_lua.lua

---
+++ scripts/vscripts/lua_abilities/troll_warlord_fervor_lua/troll_warlord_fervor_lua.lua
@@ -0,0 +1,10 @@
+troll_warlord_fervor_lua = class({})
+LinkLuaModifier( "modifier_troll_warlord_fervor_lua", "lua_abilities/troll_warlord_fervor_lua/modifier_troll_warlord_fervor_lua", LUA_MODIFIER_MOTION_NONE )
+
+--------------------------------------------------------------------------------
+
+function troll_warlord_fervor_lua:GetIntrinsicModifierName()
+	return "modifier_troll_warlord_fervor_lua"
+end
+
+--------------------------------------------------------------------------------
