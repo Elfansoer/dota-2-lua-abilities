1d814d52287164b12a61825ee369a7938e033a05
Elfansoer <elfansoer@gmail.com>
2018-01-29 16:31:39 +0700

    Create modifier_wraith_king_hellfire_blast_lua.lua

---
+++ scripts/vscripts/lua_abilities/wraith_king_hellfire_blast_lua/modifier_wraith_king_hellfire_blast_lua.lua
@@ -0,0 +1,53 @@
+modifier_wraith_king_hellfire_blast_lua = class({})
+
+--------------------------------------------------------------------------------
+
+function modifier_wraith_king_hellfire_blast_lua:IsDebuff()
+	return true
+end
+
+--------------------------------------------------------------------------------
+
+function modifier_wraith_king_hellfire_blast_lua:IsStunDebuff()
+	return true
+end
+
+--------------------------------------------------------------------------------
+
+function modifier_wraith_king_hellfire_blast_lua:GetEffectName()
+	return "particles/generic_gameplay/generic_stunned.vpcf"
+end
+
+--------------------------------------------------------------------------------
+
+function modifier_wraith_king_hellfire_blast_lua:GetEffectAttachType()
+	return PATTACH_OVERHEAD_FOLLOW
+end
+
+--------------------------------------------------------------------------------
+
+function modifier_wraith_king_hellfire_blast_lua:DeclareFunctions()
+	local funcs = {
+		MODIFIER_PROPERTY_OVERRIDE_ANIMATION,
+	}
+
+	return funcs
+end
+
+--------------------------------------------------------------------------------
+
+function modifier_wraith_king_hellfire_blast_lua:GetOverrideAnimation( params )
+	return ACT_DOTA_DISABLED
+end
+
+--------------------------------------------------------------------------------
+
+function modifier_wraith_king_hellfire_blast_lua:CheckState()
+	local state = {
+	[MODIFIER_STATE_STUNNED] = true,
+	}
+
+	return state
+end
+
+--------------------------------------------------------------------------------
