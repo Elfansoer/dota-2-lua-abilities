63e3b9b335b80d058b1c08eb7884059246072f8e
unknown <fathul.alfansuri@iti-00329.jatis.com>
2018-02-06 17:05:42 +0700

    remove files not included in tutorials

--- scripts/vscripts/lua_abilities/ursa_fury_swipes_lua/ursa_fury_swipes_lua.lua
+++
@@ -1,9 +0,0 @@
-ursa_fury_swipes_lua = class({})
-LinkLuaModifier( "modifier_ursa_fury_swipes_lua", "lua_abilities/ursa_fury_swipes_lua/modifier_ursa_fury_swipes_lua", LUA_MODIFIER_MOTION_NONE )
-LinkLuaModifier( "modifier_ursa_fury_swipes_debuff_lua", "lua_abilities/ursa_fury_swipes_lua/modifier_ursa_fury_swipes_debuff_lua", LUA_MODIFIER_MOTION_NONE )
-
---------------------------------------------------------------------------------
--- Passive Modifier
-function ursa_fury_swipes_lua:GetIntrinsicModifierName()
-	return "modifier_ursa_fury_swipes_lua"
-end
