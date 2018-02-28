4df67fa1245a1058474936bcd164977d1357cc7d
Elfansoer <elfansoer@gmail.com>
2018-02-28 17:42:17 +0700

    Create slardar_bash_of_the_deep_lua.lua

---
+++ scripts/vscripts/lua_abilities/slardar_bash_of_the_deep_lua/slardar_bash_of_the_deep_lua.lua
@@ -0,0 +1,8 @@
+slardar_bash_of_the_deep_lua = class({})
+LinkLuaModifier( "modifier_slardar_bash_of_the_deep_lua", "lua_abilities/slardar_bash_of_the_deep_lua/modifier_slardar_bash_of_the_deep_lua", LUA_MODIFIER_MOTION_NONE )
+
+--------------------------------------------------------------------------------
+-- Passive Modifier
+function slardar_bash_of_the_deep_lua:GetIntrinsicModifierName()
+	return "modifier_slardar_bash_of_the_deep_lua"
+end
