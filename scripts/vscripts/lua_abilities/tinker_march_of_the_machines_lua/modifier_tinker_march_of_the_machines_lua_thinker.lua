257155270451845426f0e1ddeb5cc44d1f47b53a
Elfansoer <elfansoer@gmail.com>
2018-04-13 11:25:57 +0700

    finalized tinker abilities

--- scripts/vscripts/lua_abilities/tinker_march_of_the_machines_lua/modifier_tinker_march_of_the_machines_lua_thinker.lua
+++ scripts/vscripts/lua_abilities/tinker_march_of_the_machines_lua/modifier_tinker_march_of_the_machines_lua_thinker.lua
@@ -19,33 +19,43 @@ end
 function modifier_tinker_march_of_the_machines_lua_thinker:OnCreated( kv )
 	if IsServer() then
 		-- references
-		self.duration = self:GetAbility():GetSpecialValueFor( "duration" ) -- special value
+		local duration = self:GetAbility():GetSpecialValueFor( "duration" ) -- special value
 		self.radius = self:GetAbility():GetSpecialValueFor( "radius" ) -- special value
 		
+		self.speed = self:GetAbility():GetSpecialValueFor( "speed" ) -- special value
 		self.distance = self:GetAbility():GetSpecialValueFor( "distance" ) -- special value
-		self.distance_scepter = self:GetAbility():GetSpecialValueFor( "distance_scepter" ) -- special value
+		if self:GetParent():HasScepter() then
+			self.distance = self:GetAbility():GetSpecialValueFor( "distance_scepter" ) -- special value
+		end
 
-		self.machines_per_sec = self:GetAbility():GetSpecialValueFor( "machines_per_sec`" ) -- special value
+		local machines_per_sec = self:GetAbility():GetSpecialValueFor( "machines_per_sec" ) -- special value
 		self.collision_radius = self:GetAbility():GetSpecialValueFor( "collision_radius" ) -- special value
 		self.splash_radius = self:GetAbility():GetSpecialValueFor( "splash_radius" ) -- special value
 		self.splash_damage = self:GetAbility():GetAbilityDamage()
 
 		-- generate Data
-		local interval = 1/self.machines_per_sec
+		self.projectile_name = "particles/econ/items/tinker/tinker_motm_rollermaw/tinker_rollermaw.vpcf"
+		local interval = 1/machines_per_sec
 		local center = self:GetParent():GetOrigin()
-		local direction = (center-self:GetCaster():GetOrigin())
-		direction = Vector( direction.x, direction.y, 0 ):Normalized()
-		-- spawn_vector = todo here
-		local center_start = center - direction*self.radius
 
-		-- add dummy ability
+		self.direction = (center-self:GetCaster():GetOrigin())
+		self.direction = Vector( self.direction.x, self.direction.y, 0 ):Normalized()
+		self:GetParent():SetForwardVector( self.direction )
+		
+		self.spawn_vector = self:GetParent():GetRightVector()
+
+		self.center_start = center - self.direction*self.radius
 
 		-- add duration
-		self:SetDuration( self.duration, false )
+		self:SetDuration( duration, false )
 
 		-- Start interval
 		self:StartIntervalThink( interval )
 		self:OnIntervalThink()
+
+		-- effects
+		local sound_cast = "Hero_Tinker.March_of_the_Machines"
+		EmitSoundOn( sound_cast, self:GetParent() )
 	end
 end
 
@@ -55,11 +65,42 @@ end
 
 function modifier_tinker_march_of_the_machines_lua_thinker:OnDestroy( kv )
 	if IsServer() then
-		UTIL_Remove( self:GeParent() )
+		-- effects
+		local sound_cast = "Hero_Tinker.March_of_the_Machines"
+		StopSoundOn( sound_cast, self:GetParent() )
+
+		UTIL_Remove( self:GetParent() )
 	end
 end
 
 --------------------------------------------------------------------------------
 -- Interval Effects
 function modifier_tinker_march_of_the_machines_lua_thinker:OnIntervalThink()
+	-- generate spawn point
+	local spawn = self.center_start + self.spawn_vector*RandomInt( -self.radius, self.radius )
+
+	-- spawn machines
+	local info = {
+		Source = self:GetCaster(),
+		Ability = self:GetAbility(),
+		vSpawnOrigin = spawn,
+		
+	    bDeleteOnHit = true,
+	    
+	    iUnitTargetTeam = DOTA_UNIT_TARGET_TEAM_ENEMY,
+	    iUnitTargetFlags = DOTA_UNIT_TARGET_FLAG_NONE,
+	    iUnitTargetType = DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
+	    
+	    EffectName = self.projectile_name,
+	    fDistance = self.distance,
+	    fStartRadius = self.collision_radius,
+	    fEndRadius =self.collision_radius,
+		vVelocity = self.direction * self.speed,
+
+		ExtraData = {
+			radius = self.splash_radius,
+			damage = self.splash_damage,
+		}
+	}
+	projectile = ProjectileManager:CreateLinearProjectile(info)
 end
--- scripts/vscripts/lua_abilities/tinker_march_of_the_machines_lua/tinker_march_of_the_machines_lua.lua
+++ scripts/vscripts/lua_abilities/tinker_march_of_the_machines_lua/tinker_march_of_the_machines_lua.lua
@@ -20,38 +20,61 @@ function tinker_march_of_the_machines_lua:OnSpellStart()
 	)
 
 	-- Play effects
+	self:PlayEffects()
 end
 --------------------------------------------------------------------------------
 -- Projectile
 function tinker_march_of_the_machines_lua:OnProjectileHit_ExtraData( target, location, extraData )
+	if not target then return true end
 
+	-- find units in radius
+	local enemies = FindUnitsInRadius(
+			self:GetCaster():GetTeamNumber(),	-- int, your team number
+			location,	-- point, center point
+			nil,	-- handle, cacheUnit. (not known)
+			extraData.radius,	-- float, radius. or use FIND_UNITS_EVERYWHERE
+			DOTA_UNIT_TARGET_TEAM_ENEMY,	-- int, team filter
+			DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,	-- int, type filter
+			0,	-- int, flag filter
+			0,	-- int, order filter
+			false	-- bool, can grow cache
+		)
+
+	-- explode
+	local damageTable = {
+		-- victim = target,
+		attacker = self:GetCaster(),
+		damage = extraData.damage,
+		damage_type = DAMAGE_TYPE_MAGICAL,
+		ability = self, --Optional.
+	}
+	for _,enemy in pairs(enemies) do
+		damageTable.victim = enemy
+		ApplyDamage(damageTable)
+	end
+
+	return true
 end
 
 --------------------------------------------------------------------------------
 function tinker_march_of_the_machines_lua:PlayEffects()
 	-- Get Resources
-	local particle_cast = "string"
-	local sound_cast = "string"
-
-	-- Get Data
+	local particle_cast = "particles/units/heroes/hero_tinker/tinker_motm.vpcf"
+	local sound_cast = "Hero_Tinker.March_of_the_Machines.Cast"
 
 	-- Create Particle
-	local effect_cast = ParticleManager:CreateParticle( particle_cast, PATTACH_NAME, hOwner )
-	ParticleManager:SetParticleControl( effect_cast, iControlPoint, vControlVector )
+	local effect_cast = ParticleManager:CreateParticle( particle_cast, PATTACH_ABSORIGIN_FOLLOW, self:GetCaster() )
 	ParticleManager:SetParticleControlEnt(
 		effect_cast,
-		iControlPoint,
-		hTarget,
-		PATTACH_NAME,
-		"attach_name",
-		vOrigin, -- unknown
-		bool -- unknown, true
+		0,
+		self:GetCaster(),
+		PATTACH_POINT_FOLLOW,
+		"attach_attack1",
+		Vector(0,0,0), -- unknown
+		true -- unknown, true
 	)
-	ParticleManager:SetParticleControlForward( effect_cast, iControlPoint, vForward )
-	SetParticleControlOrientation( effect_cast, iControlPoint, vForward, vRight, vUp )
 	ParticleManager:ReleaseParticleIndex( effect_cast )
 
 	-- Create Sound
-	EmitSoundOnLocationWithCaster( vTargetPosition, sound_location, self:GetCaster() )
-	EmitSoundOn( sound_target, target )
+	EmitSoundOnLocationForAllies( self:GetCaster():GetOrigin(), sound_cast, self:GetCaster() )
 end
