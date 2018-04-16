fb5afb8f5e329728dfd4881d524f00abbd9a628b
Elfansoer <elfansoer@gmail.com>
2018-04-16 09:51:47 +0700

    complete rework

--- scripts/vscripts/lua_abilities/_test/test.lua
+++
@@ -1,8 +0,0 @@
-test_lua = class({})
-LinkLuaModifier( "modifier_test", "lua_abilities/test/modifier_test", LUA_MODIFIER_MOTION_NONE )
-
---------------------------------------------------------------------------------
--- Passive Modifier
-function test_lua:GetIntrinsicModifierName()
-	return "modifier_test"
-end
