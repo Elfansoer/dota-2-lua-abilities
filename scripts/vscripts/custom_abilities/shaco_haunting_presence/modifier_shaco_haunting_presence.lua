ae47e083f11ac2cdc5d74e93dc7452414650e7a5
Elfansoer <elfansoer@gmail.com>
2018-05-16 17:37:56 +0700

    initiate shaco abilities

---
+++ scripts/vscripts/custom_abilities/shaco_haunting_presence/modifier_shaco_haunting_presence.lua
@@ -0,0 +1,153 @@
+modifier_shaco_haunting_presence = class({})
+local tempTable = require("util/tempTable")
+
+--------------------------------------------------------------------------------
+-- Classifications
+function modifier_shaco_haunting_presence:IsHidden()
+	return false
+end
+
+function modifier_shaco_haunting_presence:IsDebuff()
+	return false
+end
+
+function modifier_shaco_haunting_presence:IsPurgable()
+	return false
+end
+
+--------------------------------------------------------------------------------
+-- Initializations
+function modifier_shaco_haunting_presence:OnCreated( kv )
+	if IsServer() then
+		-- references
+		self.target = tempTable:RetATValue(kv.target)
+		self.distance = self:GetAbility():GetSpecialValueFor( "distance" ) -- special value
+
+		-- find angle from forward
+		local facing_angle = self.target:GetAnglesAsVector().y
+		local starting_angle = VectorToAngles(self:GetParent():GetOrigin()-self.target:GetOrigin()).y
+		self.angle_diff = AngleDiff( facing_angle, starting_angle )
+		print("facing, starting, diff:",facing_angle, starting_angle, self.angle_diff)
+
+		-- start motion
+		if self:ApplyHorizontalMotionController() == false then
+			self:Destroy()
+		end
+	end
+end
+
+function modifier_shaco_haunting_presence:OnRefresh( kv )
+	
+end
+
+function modifier_shaco_haunting_presence:OnDestroy( kv )
+	if IsServer() then
+		-- IMPORTANT: this is a must, or else the game will crash!
+		self:GetParent():InterruptMotionControllers( true )
+	end
+end
+
+--------------------------------------------------------------------------------
+-- Motion Effects
+function modifier_shaco_haunting_presence:UpdateHorizontalMotion( me, dt )
+	if IsServer() then
+		-- cancel cases
+		if self:GetParent():IsStunned() then
+			self:Destroy()
+		end
+
+		-- get relative position
+		local forward_angle = VectorToAngles(self.target:GetForwardVector()).y
+		local target_angle = forward_angle + self.angle_diff
+		if target_angle > 360 then target_angle = target_angle - 360 end
+		target_angle = math.rad(target_angle)
+		local target_vec = Vector( math.cos(forward_angle), math.sin(forward_angle), 0 ):Normalized() * 128
+
+
+		-- actual position
+		target_vec = self.target:GetOrigin() + target_vec
+
+		-- move to position
+		self:GetParent():SetOrigin( target_vec )
+	end
+end
+
+function modifier_shaco_haunting_presence:OnHorizontalMotionInterrupted()
+	if IsServer() then
+		self:Destroy()
+	end
+end
+
+--------------------------------------------------------------------------------
+-- Modifier Effects
+-- function modifier_shaco_haunting_presence:DeclareFunctions()
+-- 	local funcs = {
+-- 		MODIFIER_EVENT_ON_STATE_CHANGED,
+-- 	}
+
+-- 	return funcs
+-- end
+
+-- function modifier_shaco_haunting_presence:OnStateChanged( params )
+-- 	if IsServer() then
+-- 		if 
+-- 	end
+-- end
+
+--------------------------------------------------------------------------------
+-- Status Effects
+-- function modifier_shaco_haunting_presence:CheckState()
+-- 	local state = {
+-- 	[MODIFIER_STATE_XX] = true,
+-- 	}
+
+-- 	return state
+-- end
+
+--------------------------------------------------------------------------------
+-- Graphics & Animations
+-- function modifier_shaco_haunting_presence:GetEffectName()
+-- 	return "particles/string/here.vpcf"
+-- end
+
+-- function modifier_shaco_haunting_presence:GetEffectAttachType()
+-- 	return PATTACH_ABSORIGIN_FOLLOW
+-- end
+
+-- function modifier_shaco_haunting_presence:PlayEffects()
+-- 	-- Get Resources
+-- 	local particle_cast = "string"
+-- 	local sound_cast = "string"
+
+-- 	-- Get Data
+
+-- 	-- Create Particle
+-- 	local effect_cast = ParticleManager:CreateParticle( particle_cast, PATTACH_NAME, hOwner )
+-- 	ParticleManager:SetParticleControl( effect_cast, iControlPoint, vControlVector )
+-- 	ParticleManager:SetParticleControlEnt(
+-- 		effect_cast,
+-- 		iControlPoint,
+-- 		hTarget,
+-- 		PATTACH_NAME,
+-- 		"attach_name",
+-- 		vOrigin, -- unknown
+-- 		bool -- unknown, true
+-- 	)
+-- 	ParticleManager:SetParticleControlForward( effect_cast, iControlPoint, vForward )
+-- 	SetParticleControlOrientation( effect_cast, iControlPoint, vForward, vRight, vUp )
+-- 	ParticleManager:ReleaseParticleIndex( effect_cast )
+
+-- 	-- buff particle
+-- 	self:AddParticle(
+-- 		nFXIndex,
+-- 		bDestroyImmediately,
+-- 		bStatusEffect,
+-- 		iPriority,
+-- 		bHeroEffect,
+-- 		bOverheadEffect
+-- 	)
+
+-- 	-- Create Sound
+-- 	EmitSoundOnLocationWithCaster( vTargetPosition, sound_location, self:GetCaster() )
+-- 	EmitSoundOn( sound_target, target )
+-- end
---
+++ scripts/vscripts/custom_abilities/shaco_haunting_presence/shaco_haunting_presence.lua
@@ -0,0 +1,54 @@
+shaco_haunting_presence = class({})
+LinkLuaModifier( "modifier_shaco_haunting_presence", "custom_abilities/shaco_haunting_presence/modifier_shaco_haunting_presence", LUA_MODIFIER_MOTION_HORIZONTAL )
+local tempTable = require("util/tempTable")
+--------------------------------------------------------------------------------
+-- Ability Start
+function shaco_haunting_presence:OnSpellStart()
+	-- unit identifier
+	local caster = self:GetCaster()
+	local target = self:GetCursorTarget()
+
+	-- load data
+	local duration = self:GetSpecialValueFor("duration")
+
+	-- add modifier
+	enemy = tempTable:AddATValue(target)
+	caster:AddNewModifier(
+		caster, -- player source
+		self, -- ability source
+		"modifier_shaco_haunting_presence", -- modifier name
+		{ 
+			duration = duration,
+			target = enemy,
+		} -- kv
+	)
+end
+
+--------------------------------------------------------------------------------
+function shaco_haunting_presence:PlayEffects()
+	-- Get Resources
+	local particle_cast = "string"
+	local sound_cast = "string"
+
+	-- Get Data
+
+	-- Create Particle
+	local effect_cast = ParticleManager:CreateParticle( particle_cast, PATTACH_NAME, hOwner )
+	ParticleManager:SetParticleControl( effect_cast, iControlPoint, vControlVector )
+	ParticleManager:SetParticleControlEnt(
+		effect_cast,
+		iControlPoint,
+		hTarget,
+		PATTACH_NAME,
+		"attach_name",
+		vOrigin, -- unknown
+		bool -- unknown, true
+	)
+	ParticleManager:SetParticleControlForward( effect_cast, iControlPoint, vForward )
+	SetParticleControlOrientation( effect_cast, iControlPoint, vForward, vRight, vUp )
+	ParticleManager:ReleaseParticleIndex( effect_cast )
+
+	-- Create Sound
+	EmitSoundOnLocationWithCaster( vTargetPosition, sound_location, self:GetCaster() )
+	EmitSoundOn( sound_target, target )
+end
