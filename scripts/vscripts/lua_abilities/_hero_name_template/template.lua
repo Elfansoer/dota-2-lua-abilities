922197c98146a26576c1571546bf0d095556e6c7
unknown <elfansoer@gmail.com>
2018-10-02 19:47:40 +0700

    refresh library templates

--- scripts/vscripts/lua_abilities/_hero_name_template/template.lua
+++ scripts/vscripts/lua_abilities/_hero_name_template/template.lua
@@ -1,3 +1,14 @@
+-- Created by Elfansoer
+--[[
+Ability checklist (erase if done/checked):
+- Scepter Upgrade
+- Break behavior
+- Linken/Reflect behavior
+- Spell Immune/Invulnerable/Invisible behavior
+- Illusion behavior
+- Stolen behavior
+]]
+--------------------------------------------------------------------------------
 template = class({})
 LinkLuaModifier( "modifier_template", "lua_abilities/template/modifier_template", LUA_MODIFIER_MOTION_NONE )
 
@@ -79,31 +90,9 @@ function template:OnProjectileHit( target, location )
 end
 
 --------------------------------------------------------------------------------
--- Ability Considerations
-function template:AbilityConsiderations()
-	-- Scepter
-	local bScepter = caster:HasScepter()
-
-	-- Linken & Lotus
-	local bBlocked = target:TriggerSpellAbsorb( self )
-
-	-- Break
-	local bBroken = caster:PassivesDisabled()
-
-	-- Advanced Status
-	local bInvulnerable = target:IsInvulnerable()
-	local bInvisible = target:IsInvisible()
-	local bHexed = target:IsHexed()
-	local bMagicImmune = target:IsMagicImmune()
-
-	-- Illusion Copy
-	local bIllusion = target:IsIllusion()
-end
-
---------------------------------------------------------------------------------
 function template:PlayEffects()
 	-- Get Resources
-	local particle_cast = "string"
+	local particle_cast = "particles/units/heroes/hero_heroname/heroname_ability.vpcf"
 	local sound_cast = "string"
 
 	-- Get Data
@@ -177,131 +166,4 @@ end
 -- Other Events
 function template:OnHeroDiedNearby(handle unit, handle attacker, handle table)
 
-end
-
---------------------------------------------------------------------------------
--- Built-in functions
--- Helper: Ability Table (AT)
-function template:GetAT()
-	if self.abilityTable==nil then
-		self.abilityTable = {}
-	end
-	return self.abilityTable
-end
-
-function template:GetATEmptyKey()
-	local table = self:GetAT()
-	local i = 1
-	while table[i]~=nil do
-		i = i+1
-	end
-	return i
-end
-
-function template:AddATValue( value )
-	local table = self:GetAT()
-	local i = self:GetATEmptyKey()
-	table[i] = value
-	return i
-end
-
-function template:RetATValue( key )
-	local table = self:GetAT()
-	local ret = table[key]
-	table[key] = nil
-	return ret
-end
-
--- Helper: Flag operations
-function template:FlagExist(a,b)--Bitwise Exist
-	local p,c,d=1,0,b
-	while a>0 and b>0 do
-		local ra,rb=a%2,b%2
-		if ra+rb>1 then c=c+p end
-		a,b,p=(a-ra)/2,(b-rb)/2,p*2
-	end
-	return c==d
-end
-
-function template:FlagAdd(a,b)--Bitwise and
-	if FlagExist(a,b) then
-		return a
-	else
-		return a+b
-	end
-end
-
-function template:FlagMin(a,b)--Bitwise and
-	if FlagExist(a,b) then
-		return a-b
-	else
-		return a
-	end
-end
-
--- Helper: Bitwise operations
-function template:BitXOR(a,b)--Bitwise xor
-    local p,c=1,0
-    while a>0 and b>0 do
-        local ra,rb=a%2,b%2
-        if ra~=rb then c=c+p end
-        a,b,p=(a-ra)/2,(b-rb)/2,p*2
-    end
-    if a<b then a=b end
-    while a>0 do
-        local ra=a%2
-        if ra>0 then c=c+p end
-        a,p=(a-ra)/2,p*2
-    end
-    return c
-end
-
-function template:BitOR(a,b)--Bitwise or
-    local p,c=1,0
-    while a+b>0 do
-        local ra,rb=a%2,b%2
-        if ra+rb>0 then c=c+p end
-        a,b,p=(a-ra)/2,(b-rb)/2,p*2
-    end
-    return c
-end
-
-function template:BitNOT(n)
-    local p,c=1,0
-    while n>0 do
-        local r=n%2
-        if r<1 then c=c+p end
-        n,p=(n-r)/2,p*2
-    end
-    return c
-end
-
-function template:BitAND(a,b)--Bitwise and
-    local p,c=1,0
-    while a>0 and b>0 do
-        local ra,rb=a%2,b%2
-        if ra+rb>1 then c=c+p end
-        a,b,p=(a-ra)/2,(b-rb)/2,p*2
-    end
-    return c
-end
-
-function azura_shadowform:PrintTableRecursive( key, value, tab )
-	-- abort deep recursive
-	if tab>10 then
-		print("quit")
-		return
-	end
-
-	local tabText = ""
-	for i=0,tab do
-		tabText = tabText .. "\t"
-	end
-
-	print(tabText,key,value, type(value))
-	if (type(value)=='table') then
-		for k,v in pairs(value) do
-			self:PrintTableRecursive( k, v, tab + 1)
-		end
-	end
 end
---
+++ scripts/vscripts/util/flag.lua
@@ -0,0 +1,34 @@
+--------------------------------------------------------------------------------
+-- Helper: flag Calculations
+if not flag then
+	flag = {}
+end
+
+-- Returns true if flag b is within state a
+function flag:Exist(a,b)
+	local p,c,d=1,0,b
+	while a>0 and b>0 do
+		local ra,rb=a%2,b%2
+		if ra+rb>1 then c=c+p end
+		a,b,p=(a-ra)/2,(b-rb)/2,p*2
+	end
+	return c==d
+end
+
+-- Adds flag b to state a, if not exist
+function flag:Add(a,b)
+	if flagExist(a,b) then
+		return a
+	else
+		return a+b
+	end
+end
+
+-- Removes flag b to state a, if exist
+function flag:Min(a,b)
+	if flagExist(a,b) then
+		return a-b
+	else
+		return a
+	end
+end
