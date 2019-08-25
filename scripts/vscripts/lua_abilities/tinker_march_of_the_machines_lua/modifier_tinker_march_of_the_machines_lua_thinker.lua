fae3aa8e54c85db2d94fe010b79d1fad685d823c
Woo Huiren <giantcrabby@gmail.com>
2019-08-25 18:26:43 +0800

    Fix direction reference

--- scripts/vscripts/lua_abilities/tinker_march_of_the_machines_lua/modifier_tinker_march_of_the_machines_lua_thinker.lua
+++ scripts/vscripts/lua_abilities/tinker_march_of_the_machines_lua/modifier_tinker_march_of_the_machines_lua_thinker.lua
@@ -62,7 +62,7 @@ function modifier_tinker_march_of_the_machines_lua_thinker:OnCreated( kv )
 		    fDistance = distance,
 		    fStartRadius = collision_radius,
 		    fEndRadius = collision_radius,
-			vVelocity = self.direction * speed,
+			vVelocity = direction * speed,
 
 			ExtraData = {
 				radius = splash_radius,
