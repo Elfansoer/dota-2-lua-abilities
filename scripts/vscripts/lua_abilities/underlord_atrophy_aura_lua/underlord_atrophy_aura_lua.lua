f090a02547399d58a76c12ac5966490316386db1
unknown <elfansoer@gmail.com>
2020-04-30 14:21:28 +0900

    updated atrophy aura

--- scripts/vscripts/lua_abilities/underlord_atrophy_aura_lua/underlord_atrophy_aura_lua.lua
+++ scripts/vscripts/lua_abilities/underlord_atrophy_aura_lua/underlord_atrophy_aura_lua.lua
@@ -18,23 +18,6 @@ LinkLuaModifier( "modifier_underlord_atrophy_aura_lua_scepter", "lua_abilities/u
 
 --------------------------------------------------------------------------------
 -- Passive Modifier
--- function underlord_atrophy_aura_lua:GetIntrinsicModifierName()
--- 	return "modifier_underlord_atrophy_aura_lua"
--- end
-
-function underlord_atrophy_aura_lua:GetBehavior()
-	return DOTA_ABILITY_BEHAVIOR_NO_TARGET
-end
---------------------------------------------------------------------------------
--- Ability Start
-function underlord_atrophy_aura_lua:OnSpellStart()
-	-- unit identifier
-	local caster = self:GetCaster()
-
-	caster:AddNewModifier(
-		caster, -- player source
-		self, -- ability source
-		"modifier_underlord_atrophy_aura_lua", -- modifier name
-		{ duration = 15 } -- kv
-	)
+function underlord_atrophy_aura_lua:GetIntrinsicModifierName()
+	return "modifier_underlord_atrophy_aura_lua"
 end
