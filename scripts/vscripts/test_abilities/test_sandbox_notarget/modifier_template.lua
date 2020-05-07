6c395a414489634669af5884efe41b280c27a4c2
unknown <elfansoer@gmail.com>
2020-05-07 19:15:02 +0900

    testing arc

--- scripts/vscripts/test_abilities/test_sandbox_notarget/modifier_template.lua
+++ scripts/vscripts/test_abilities/test_sandbox_notarget/modifier_template.lua
@@ -29,21 +29,46 @@ function modifier_template:IsPurgable()
 	return true
 end
 
-function modifier_template:GetAttributes()
-	return MODIFIER_ATTRIBUTE_MULTIPLE + MODIFIER_ATTRIBUTE_INVULNERABLE 
-end
-
 --------------------------------------------------------------------------------
 -- Initializations
 function modifier_template:OnCreated( kv )
-	-- references
-	self.special_value = self:GetAbility():GetSpecialValueFor( "special_value" )
 
-	if IsServer() then
-		-- Start interval
-		self:StartIntervalThink( self.interval )
-		self:OnIntervalThink()
-	end
+	if not IsServer() then return end
+
+	self.target = EntIndexToHScript( kv.tree )
+	local speed = kv.speed
+
+	-- references
+	self.origin = self:GetParent():GetOrigin()
+
+	local vec = self.target:GetOrigin() - self.origin
+	vec.z = 0
+
+	self.distance = vec:Length2D()
+	self.duration = self.distance/speed
+	self.height = 192
+	self.direction = vec:Normalized()
+
+	-- horizontal motion
+	-- self.duration = 0.45
+	-- self.distance = 550
+	-- self.height = 150
+	-- self.direction = self:GetParent():GetForwardVector()
+
+	-- obtain final height
+	local pos = self.target:GetOrigin()
+	local ground = GetGroundHeight( pos, self:GetParent() )
+	local diff = 128 + ground - self.origin.z
+
+
+	self:InitVerticalArc( self.height, diff, self.duration )
+
+	self:SetDuration( self.duration, true )
+	self:ApplyHorizontalMotionController()
+	self:ApplyVerticalMotionController()
+
+	self.total1 = 0
+	self.total2 = 0
 end
 
 function modifier_template:OnRefresh( kv )
@@ -54,128 +79,130 @@ function modifier_template:OnRemoved()
 end
 
 function modifier_template:OnDestroy()
-end
-
---------------------------------------------------------------------------------
--- Modifier Effects
-function modifier_template:DeclareFunctions()
-	local funcs = {
-		MODIFIER_PROPERTY_BASEATTACK_BONUSDAMAGE,
-		MODIFIER_EVENT_ON_ATTACKED,
-	}
-
-	return funcs
-end
-
---------------------------------------------------------------------------------
--- Status Effects
-function modifier_template:CheckState()
-	local state = {
-		[MODIFIER_STATE_INVULNERABLE] = true,
-	}
-
-	return state
-end
-
---------------------------------------------------------------------------------
--- Interval Effects
-function modifier_template:OnIntervalThink()
+	if not IsServer() then return end
+	self:GetParent():InterruptMotionControllers( true )
 end
 
 --------------------------------------------------------------------------------
 -- Motion Effects
 function modifier_template:UpdateHorizontalMotion( me, dt )
+	local time = self.total2/self.duration
+	local pos = self.origin + self.direction * self.distance * time
+
+	me:SetOrigin( pos )
+
+	self.total2 = self.total2 + dt
 end
 
 function modifier_template:OnHorizontalMotionInterrupted()
 end
 
+function modifier_template:UpdateVerticalMotion( me, dt )
+
+	-- -- absolute
+	-- local height = self.origin.z + self:f( self.total1 )
+	-- local pos = me:GetOrigin()
+	-- pos.z = height
+	-- me:SetOrigin( pos )
+
+	-- relative
+	local pos = me:GetOrigin()
+
+	local height = pos.z
+	local change = self:df( self.total1 )
+	pos.z = height + change * dt
+	me:SetOrigin( pos )
+
+	self.total1 = self.total1 + dt
+end
+
+function modifier_template:OnVerticalMotionInterrupted()
+end
+
 --------------------------------------------------------------------------------
--- Aura Effects
-function modifier_template:IsAura()
-	return true
+-- Helper
+function modifier_template:f(x)
+	return self.const1*x - self.const2*x*x
 end
 
-function modifier_template:GetModifierAura()
-	return "modifier_template_effect"
+function modifier_template:df(x)
+	return self.const1 - 2*self.const2*x
 end
 
-function modifier_template:GetAuraRadius()
-	return self.radius
-end
-
-function modifier_template:GetAuraDuration()
-	return self.radius
-end
-
-function modifier_template:GetAuraSearchTeam()
-	return DOTA_UNIT_TARGET_TEAM_FRIENDLY
-end
-
-function modifier_template:GetAuraSearchType()
-	return DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC
-end
-
-function modifier_template:GetAuraSearchFlags()
-	return DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES
-end
-
-function modifier_template:GetAuraEntityReject( hEntity )
-	if IsServer() then
-		
+function modifier_template:InitVerticalArc( height_max, height_end, duration )
+	-- obtain max height
+	local avg = height_end/2
+	height_max = height_max + avg
+	if height_max<height_end then
+		height_max = height_end+10
+	end
+	if height_max<=0 then
+		height_max = 10
 	end
 
-	return false
+	-- math magic
+	local duration_end = ( 1 + math.sqrt( 1 - height_end/height_max ) )/2
+	self.const1 = 4*height_max*duration_end/duration
+	self.const2 = 4*height_max*duration_end*duration_end/(duration*duration)
 end
 
---------------------------------------------------------------------------------
--- Graphics & Animations
-function modifier_template:GetEffectName()
-	return "particles/units/heroes/hero_heroname/heroname_ability.vpcf"
+function modifier_template:GetVerticalPos( time )
+	return self.const1*time - self.const2*time*time
 end
 
-function modifier_template:GetEffectAttachType()
-	return PATTACH_ABSORIGIN_FOLLOW
+function modifier_template:GetVerticalSpeed( time )
+	return self.const1 - 2*self.const2*time
 end
 
-function modifier_template:GetStatusEffectName()
-	return "status/effect/here.vpcf"
-end
 
-function modifier_template:PlayEffects()
-	-- Get Resources
-	local particle_cast = "particles/units/heroes/hero_heroname/heroname_ability.vpcf"
-	local sound_cast = "string"
+-- --------------------------------------------------------------------------------
+-- -- Graphics & Animations
+-- function modifier_template:GetEffectName()
+-- 	return "particles/units/heroes/hero_heroname/heroname_ability.vpcf"
+-- end
 
-	-- Get Data
+-- function modifier_template:GetEffectAttachType()
+-- 	return PATTACH_ABSORIGIN_FOLLOW
+-- end
 
-	-- Create Particle
-	local effect_cast = ParticleManager:CreateParticle( particle_cast, PATTACH_NAME, hOwner )
-	ParticleManager:SetParticleControl( effect_cast, iControlPoint, vControlVector )
-	ParticleManager:SetParticleControlEnt(
-		effect_cast,
-		iControlPoint,
-		hTarget,
-		PATTACH_NAME,
-		"attach_name",
-		vOrigin, -- unknown
-		bool -- unknown, true
-	)
-	ParticleManager:SetParticleControlForward( effect_cast, iControlPoint, vForward )
-	SetParticleControlOrientation( effect_cast, iControlPoint, vForward, vRight, vUp )
-	ParticleManager:ReleaseParticleIndex( effect_cast )
+-- function modifier_template:GetStatusEffectName()
+-- 	return "status/effect/here.vpcf"
+-- end
 
-	-- buff particle
-	self:AddParticle(
-		effect_cast,
-		false, -- bDestroyImmediately
-		false, -- bStatusEffect
-		-1, -- iPriority
-		false, -- bHeroEffect
-		false -- bOverheadEffect
-	)
+-- function modifier_template:PlayEffects()
+-- 	-- Get Resources
+-- 	local particle_cast = "particles/units/heroes/hero_heroname/heroname_ability.vpcf"
+-- 	local sound_cast = "string"
 
-	-- Create Sound
-	EmitSoundOnLocationWithCaster( vTargetPosition, sound_location, self:GetCaster() )
-	EmitSoundOn( sound_target, target )
-end
+-- 	-- Get Data
+
+-- 	-- Create Particle
+-- 	local effect_cast = ParticleManager:CreateParticle( particle_cast, PATTACH_NAME, hOwner )
+-- 	ParticleManager:SetParticleControl( effect_cast, iControlPoint, vControlVector )
+-- 	ParticleManager:SetParticleControlEnt(
+-- 		effect_cast,
+-- 		iControlPoint,
+-- 		hTarget,
+-- 		PATTACH_NAME,
+-- 		"attach_name",
+-- 		vOrigin, -- unknown
+-- 		bool -- unknown, true
+-- 	)
+-- 	ParticleManager:SetParticleControlForward( effect_cast, iControlPoint, vForward )
+-- 	SetParticleControlOrientation( effect_cast, iControlPoint, vForward, vRight, vUp )
+-- 	ParticleManager:ReleaseParticleIndex( effect_cast )
+
+-- 	-- buff particle
+-- 	self:AddParticle(
+-- 		effect_cast,
+-- 		false, -- bDestroyImmediately
+-- 		false, -- bStatusEffect
+-- 		-1, -- iPriority
+-- 		false, -- bHeroEffect
+-- 		false -- bOverheadEffect
+-- 	)
+
+-- 	-- Create Sound
+-- 	EmitSoundOnLocationWithCaster( vTargetPosition, sound_location, self:GetCaster() )
+-- 	EmitSoundOn( sound_target, target )
+-- end
--- scripts/vscripts/test_abilities/test_sandbox_notarget/test_sandbox_notarget.lua
+++ scripts/vscripts/test_abilities/test_sandbox_notarget/test_sandbox_notarget.lua
@@ -10,46 +10,30 @@ Ability checklist (erase if done/checked):
 ]]
 --------------------------------------------------------------------------------
 test_sandbox_notarget = class({})
-LinkLuaModifier( "modifier_test_sandbox_notarget", "test_abilities/test_sandbox_notarget/test_sandbox_notarget", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_template", "test_abilities/test_sandbox_notarget/modifier_template", LUA_MODIFIER_MOTION_BOTH )
+
+-- function test_sandbox_notarget:GetBehavior()
+-- 	return DOTA_ABILITY_BEHAVIOR_UNIT_TARGET
+-- end
 
 --------------------------------------------------------------------------------
 -- Ability Start
 function test_sandbox_notarget:OnSpellStart()
 	-- unit identifier
 	local caster = self:GetCaster()
+	local target = self:GetCursorTarget()
+	print(target:GetOrigin())
+	print(target.IsStanding, target.CutDown, target:entindex())
 
-	local enemies = FindUnitsInRadius(
-		caster:GetTeamNumber(),	-- int, your team number
-		caster:GetOrigin(),	-- point, center point
-		nil,	-- handle, cacheUnit. (not known)
-		1000,	-- float, radius. or use FIND_UNITS_EVERYWHERE
-		DOTA_UNIT_TARGET_TEAM_ENEMY,	-- int, team filter
-		DOTA_UNIT_TARGET_HERO,	-- int, type filter
-		DOTA_UNIT_TARGET_FLAG_INVULNERABLE,	-- int, flag filter
-		0,	-- int, order filter
-		false	-- bool, can grow cache
-	)
+	local speed = 700
 
-	for _,enemy in pairs(enemies) do
-		print("found enemy",enemy:GetUnitName())
-		enemy:AddNewModifier(
-			caster, -- player source
-			self, -- ability source
-			"modifier_test_sandbox_notarget", -- modifier name
-			{
-				duration = 5,
-				target = enemy:entindex(),
-			} -- kv
-		)
-		break
-	end
 	caster:AddNewModifier(
 		caster, -- player source
 		self, -- ability source
-		"modifier_test_sandbox_notarget", -- modifier name
+		"modifier_template", -- modifier name
 		{
-			duration = 5,
-			target = caster:entindex(),
+			speed = speed,
+			tree = target:entindex(),
 		} -- kv
 	)
 end
@@ -62,131 +46,4 @@ function test_sandbox_notarget:PlayEffects()
 	ParticleManager:SetParticleControl( effect_cast, 0, self:GetCaster():GetOrigin() )
 	ParticleManager:SetParticleControl( effect_cast, 1, Vector( 100, 1, 1 ) )
 	ParticleManager:ReleaseParticleIndex( effect_cast )
-end
-
-modifier_test_sandbox_notarget = class({})
---------------------------------------------------------------------------------
--- Classifications
-function modifier_test_sandbox_notarget:IsHidden()
-	return false
-end
-function modifier_test_sandbox_notarget:GetAttributes()
-	return MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE + MODIFIER_ATTRIBUTE_PERMANENT
-end
-
---------------------------------------------------------------------------------
--- Initializations
-function modifier_test_sandbox_notarget:OnCreated( kv )
-	-- references
-	-- self.special_value = self:GetAbility():GetSpecialValueFor( "special_value" )
-	if IsServer() then
-		self:SetStackCount( kv.target )
-		self.target = EntIndexToHScript( kv.target )
-	else
-		local stack = self:GetStackCount()
-		self.target = EntIndexToHScript( stack )
-	end
-	print("created")
-
-	-- if IsServer() then
-	-- 	-- Start interval
-	-- 	self:StartIntervalThink( self.interval )
-	-- 	self:OnIntervalThink()
-	-- end
-end
-
-function modifier_test_sandbox_notarget:OnRemoved()
-	if not IsServer() then return end
-	print("removed")
-end
-function modifier_test_sandbox_notarget:OnDestroy()
-	if not IsServer() then return end
-	print("destroyed")
-end
-
---------------------------------------------------------------------------------
--- Aura Effects
--- function modifier_test_sandbox_notarget:IsAura()
--- 	-- return self:GetParent()==self:GetCaster()
--- 	return false
--- end
-
--- function modifier_test_sandbox_notarget:GetModifierAura()
--- 	return "modifier_test_sandbox_notarget"
--- end
-
--- function modifier_test_sandbox_notarget:GetAuraRadius()
--- 	return FIND_UNITS_EVERYWHERE
--- end
-
--- function modifier_test_sandbox_notarget:GetAuraDuration()
--- 	return 0.5
--- end
-
--- function modifier_test_sandbox_notarget:GetAuraSearchTeam()
--- 	return DOTA_UNIT_TARGET_TEAM_ENEMY
--- end
-
--- function modifier_test_sandbox_notarget:GetAuraSearchType()
--- 	return DOTA_UNIT_TARGET_ALL
--- end
-
--- function modifier_test_sandbox_notarget:GetAuraSearchFlags()
--- 	return DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES
--- end
-
--- function modifier_test_sandbox_notarget:GetAuraEntityReject( hEntity )
--- 	if IsServer() then
-		
--- 	end
-
--- 	return false
--- end
-
---------------------------------------------------------------------------------
--- Modifier Effects
-function modifier_test_sandbox_notarget:DeclareFunctions()
-	local funcs = {
-		-- MODIFIER_PROPERTY_IGNORE_PHYSICAL_ARMOR,
-		MODIFIER_EVENT_ON_MODEL_CHANGED,
-		MODIFIER_PROPERTY_MODEL_CHANGE,
-		MODIFIER_EVENT_ON_MODIFIER_ADDED,
-	}
-
-	return funcs
-end
-
--- function modifier_test_sandbox_notarget:OnModifierAdded( params )
--- 	if not IsServer() then return end
--- 	if params.unit~=self:GetParent() then return end
--- 	print("OnModifierAdded",self:GetParent():GetUnitName(),params.unit:GetUnitName())
--- 	-- for k,v in pairs(params) do
--- 	-- 	print(k,v)
--- 	-- end
--- end
-
-function modifier_test_sandbox_notarget:OnModelChanged( params )
-	if not IsServer() then return end
-	if params.attacker~=self:GetParent() then return end
-	print( params.attacker:GetModelName() )
-end
-function modifier_test_sandbox_notarget:GetModifierModelChange( params )
-	if not IsServer() then return end
-	return self:GetCaster():GetModelName()
-end
-
-function modifier_test_sandbox_notarget:GetModifierIgnorePhysicalArmor( params )
-	if not IsServer() then return end
-	print(self:GetParent():GetUnitName(),"GetModifierIgnorePhysicalArmor")
-	for k,v in pairs(params) do
-		print(k,v)
-	end
-end
-
-function modifier_test_sandbox_notarget:GetEffectName()
-	return "particles/units/heroes/hero_nevermore/nevermore_shadowraze_debuff.vpcf"
-end
-
-function modifier_test_sandbox_notarget:GetEffectAttachType()
-	return PATTACH_ABSORIGIN_FOLLOW
 end
